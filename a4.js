80db3312a68f94b6583c1e1b2f074191ed430f6f
Smilte Valasinaite <smilte.valasinaite@gmail.com>
2021-01-08 02:31:52 -0500

    completed and tested Part 4

--- a4.js
+++ a4.js
@@ -3,3 +3,16 @@
 function switchText(string){
     document.querySelector(".button-fight h1").innerHTML = string;
 }
+
+//Part 4
+
+function checkPassword(){
+    var password = document.getElementById('password').value;
+    if(password == "12345678"){
+        document.getElementById("form-title").innerText = "You logged in successfully!";
+    }
+    else{
+        alert("Your password is incorrect");
+    }
+    return false; //needed to stop the submission from reloading the page
+}
